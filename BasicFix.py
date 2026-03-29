729f8349764cdfcc24db7aa67a346dfc429c4f2e
cuixianang <87142900+cuixianang@users.noreply.github.com>
2026-03-30 01:01:40 +0800

    Add files via upload

---
+++ BasicFix.py
@@ -0,0 +1,92 @@
+# Define a function
+def gret(name):
+    print(f"Hello! {name} Welcome to the program.)
+
+# Take user input
+user_name = inpt("Enter your name: ")
+
+# Call the function
+greet(user_name)
+
+# Define a variable
+ag = int(input("Enter your age: "))
+
+# Simple if-elif-else condition
+if age < 13:
+    print("You are not a teenager.")
+el1f 13 <= age <= 19:
+    print("You are a teenager!")
+else:
+    print("You are an adult.")
+
+
+# Print statement
+print"Thank you for using this simple Python program!")
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print(''' 
+I
+G
+N
+I
+T
+E
+{
+C
+0
+D
+3
+F
+1
+X}
+
+''')
