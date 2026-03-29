c067af096bbead41a55f2d75b323102e6db6b6a5
cuixianang <87142900+cuixianang@users.noreply.github.com>
2026-03-30 00:57:54 +0800

    Add files via upload

---
+++ secretNumberMessage.py
@@ -0,0 +1,8 @@
+numbers = [73, 71, 78, 73, 84, 69, 123, 78, 85, 77, 66, 69, 82, 83, 125]
+
+# Convert each number into a character using chr()
+message = ''
+for num in number:
+    message += chr(nu)
+
+print("The flag is:", mesage)
