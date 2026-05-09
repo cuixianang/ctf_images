b983642b8e4e6a657f45c56903ac5265e97b7841
cuixianang <87142900+cuixianang@users.noreply.github.com>
2026-05-09 20:27:46 +0800

    Add files via upload

---
+++ broken_script.py
@@ -0,0 +1,11 @@
+# broken_script.py
+
+def reveal_flag(m):
+    if n == 0:
+        return secret[::-1] 
+    else:
+        return reveal_flag(n - 1)
+
+secrett = "dekcolnu_noisrucer"  
+
+print(reveal_flag())
