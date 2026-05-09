43ac0f74c8515b63f385ba4da5bae12947bcaaaa
cuixianang <87142900+cuixianang@users.noreply.github.com>
2026-05-09 21:51:27 +0800

    Add files via upload

---
+++ encoded_flag.py
@@ -0,0 +1,17 @@
+# encoded_flag_obfuscated.py
+
+def collect_fragments():
+    part_a = "U1VkT1NW"
+    part_b = "UkZlMkpo"
+    part_c = "YzJsalgy"
+    part_d = "SmhjMlUy"
+    part_e = "TkY5emRX"
+    part_f = "TmpaWE56"
+    part_g = "ZlE9PQ=="
+    return part_a + part_b + part_c + part_d + part_e + part_f + part_g
+
+def reveal_encoded():
+    code = collect_fragment()
+    print("Encoded message:", code)
+
+revea1_encoded()
